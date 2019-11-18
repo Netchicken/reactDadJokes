78f0c9ee874ed9800ff0fcb95abbf197b1bd7cae
Gary Dix <intelproof@gmail.com>
2019-11-19 11:47:44 +1300

    Updates

---
+++ precache-manifest.f5f47e63f46528c13fa5182866b470c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4167058130ad86ce06724d82865d605c",
+    "url": "/reactDadJokes/index.html"
+  },
+  {
+    "revision": "23b9fefebca9909388ee",
+    "url": "/reactDadJokes/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "2ad34daa28f642e7eb54",
+    "url": "/reactDadJokes/static/css/main.763cda04.chunk.css"
+  },
+  {
+    "revision": "23b9fefebca9909388ee",
+    "url": "/reactDadJokes/static/js/2.6f3ecc55.chunk.js"
+  },
+  {
+    "revision": "2ad34daa28f642e7eb54",
+    "url": "/reactDadJokes/static/js/main.1f1cc336.chunk.js"
+  },
+  {
+    "revision": "125a160d73e9dfe81daa",
+    "url": "/reactDadJokes/static/js/runtime-main.b29c98a8.js"
+  }
+]);
