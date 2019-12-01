c20ec12952a92969f957cbcb36d580bb6671a2d4
Gary Dix <intelproof@gmail.com>
2019-12-02 09:14:00 +1300

    Update AppHeader

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,43 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
+//https://reactstrap.github.io/components/navbar/
+const AppHeader = props => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const AppHeader = () => {
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <header>
-      <nav class="navbar navbar-expand-lg navbar-dark default-color">
-        <a class="navbar-brand" href="https://visioncollege.ac.nz/">
-          <strong>Vision College Dad Jokes</strong>
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+    <div className="container-fluid  justify-content-md-center App-header ">
+      <Navbar color="rgb(169, 185, 124)" light expand="sm">
+        <NavbarBrand
+          className="NavbarBrand"
+          href="https://visioncollege.ac.nz/"
         >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="https://visioncollege.ac.nz/">
-                College Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                class="nav-link"
-                href="https://visioncollege.ac.nz/study/information-technology/"
-              >
+          Guess The Definition
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://visioncollege.ac.nz/study/information-technology/">
                 Information Technology Courses
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+              </NavLink>
+            </NavItem>
+            {
+              <NavItem>
+                <NavLink href="https://visioncollege.ac.nz/">
+                  Vision College Home
+                </NavLink>
+              </NavItem>
+            }
+            {/* <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown> */}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
 export default AppHeader;
+
